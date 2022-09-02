713c03cac98f626909720dde19175f083f796872
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 <tusharpatil22700@gmail.com>
2022-09-01 18:50:07 -0700

    first commit

---
+++ src/bridgelabz/com/LineCompaison.java
@@ -0,0 +1,18 @@
+package bridgelabz.com;
+
+public class LineCompaison {
+	public static void main(String args[]) {
+		
+			int x1,x2,y1,y2;
+			x1=2;x2=5;
+			y1=4;y2=8;
+			
+			double distance=Math.sqrt(x2-x1)*2 +(y2-y1)*2;
+			System.out.println("distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+distance);
+			
+			
+	}
+	
+	
+
+}
