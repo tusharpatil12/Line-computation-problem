c62f3430f6812f70ec313c127bccbf84e14a4907
tusharpatil12 <110172800+tusharpatil12@users.noreply.github.com>
2022-10-20 00:19:21 -0700

    Update LineCompaison.java

--- src/bridgelabz/com/LineCompaison.java
+++ src/bridgelabz/com/LineCompaison.java
@@ -1,18 +1,13 @@
 package bridgelabz.com;
 
 public class LineCompaison {
+	static void length(int x1, int y1, int x2, int y2) {
+		double Line = Math.sqrt((x2-x2)*(x2-x1)+(y2-y1)*(y2-y1));
+		System.out.println(Line);
+
 	public static void main(String args[]) {
-		
-			int x1,x2,y1,y2;
-			x1=2;x2=5;
-			y1=4;y2=8;
-			
-			double distance=Math.sqrt(x2-x1)*2 +(y2-y1)*2;
-			System.out.println("distance between = "+"("+x1+","+y1+"),"+"("+x2+","+y2+") "+distance);
-			
-			
+		System.out.println("Welcome to Line Comparison");
+		length(1,1,3,3);		
 	}
-	
-	
 
 }
